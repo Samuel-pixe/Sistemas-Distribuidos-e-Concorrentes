aa09dc7d0d600a81fba457a7e60b7ca8d50d0d87
Samuel Arantes <65547548+Samuel-pixe@users.noreply.github.com>
2023-09-05 10:35:24 -0300

    At.08.09.2023

---
+++ Sockets-v4/src/tarefa01/ClienteServidor.java
@@ -0,0 +1,125 @@
+package tarefa01;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+public class ClienteServidor extends Thread {
+
+    private final String HOST = "127.0.0.1";
+    private final int PORTA_ENVIAR;
+    private final int PORTA_RECEBER;
+    private final JTextArea txt;
+
+    public ClienteServidor(JTextArea txt, int PORTA_ENVIAR, int PORTA_RECEBER) {
+        this.txt = txt;
+        this.PORTA_ENVIAR = PORTA_ENVIAR;
+        this.PORTA_RECEBER = PORTA_RECEBER;
+        txt.append("\nAplicação executando na porta " + PORTA_RECEBER);
+    }
+
+    public void enviar(String msg) {
+        try {
+            //converter a mensagem em bytes
+            byte[] buffer = msg.getBytes();
+
+            //criar o pacote da mensagem
+            DatagramPacket pct = new DatagramPacket(
+                    buffer,
+                    buffer.length,
+                    InetAddress.getByName(HOST),
+                    PORTA_ENVIAR
+            );
+
+            //enviar a mensagem
+            new DatagramSocket().send(pct);
+
+        } catch (IOException e) {
+            System.err.println("ERRO: " + e.getMessage());
+        }
+    }
+    
+    
+
+    @Override
+    public void run() {
+        try {
+            DatagramSocket s = new DatagramSocket(PORTA_RECEBER);
+            while (true) {
+                byte[] buffer = new byte[256];
+                DatagramPacket pct = new DatagramPacket(
+                        buffer, buffer.length
+                );
+                s.receive(pct);
+
+                String msg = new String(pct.getData()).trim();
+
+                //System.out.println(msg);
+                if (msg.equals("DATA")) {
+                    LocalDateTime dataAtual = ClienteServidor.DataAtualDoServidor.obterDataAtual();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");// Definindo o padrão brasileiro
+                    //String mensagemData = "Data atual do servidor: " + dataAtual.toString();
+                    String dataFormatada = dataAtual.format(formatter);
+
+                    String mensagemData = "Data atual do servidor: " + dataFormatada;
+                    enviar(mensagemData);                                  
+                    // Formatando a data no padrão brasileiro                          
+                    
+                }
+                if (msg.equals("DATA2")) {                               
+                    LocalDateTime dataAtual = LocalDateTime.now();   
+                    // Defina um formato com padrão brasileiro e locale em português
+                    DateTimeFormatter formato = DateTimeFormatter.ofPattern("d 'de' MMMM 'de' yyyy 'às' HH:mm:ss", new Locale("pt", "BR"));
+                    // Formate a data por extenso
+                    String dataPorExtenso = dataAtual.format(formato);
+                    String mensagemData = "Data atual do servidor: " + dataPorExtenso;
+                    enviar(mensagemData);
+                    
+                }
+                if (msg.equals("HORA")) {                               
+                    LocalDateTime dataAtual = LocalDateTime.now();
+                    // Defina um formato para a hora (HH:mm:ss)
+                    DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss");
+                    // Formate apenas a hora
+                    String horaFormatada = dataAtual.format(formato);
+                    String mensagemHora = "Hora atual do servidor: " + horaFormatada;
+                    enviar(mensagemHora);                 
+                }
+                if(msg.equals("DIAS-RESTANTES")){
+                    LocalDate dataAtual = LocalDate.now();
+                    // Obtém a data do último dia do ano atual
+                    LocalDate ultimoDiaDoAno = LocalDate.of(dataAtual.getYear(), 12, 31);
+
+                    // Calcula os dias restantes até o fim do ano
+                    long diasRestantes = ChronoUnit.DAYS.between(dataAtual, ultimoDiaDoAno);
+                    String texto = String.valueOf(diasRestantes);
+                    enviar(texto);
+                    // Exibe a quantidade de dias restantes
+                    
+                    } 
+                
+                txt.append("\n");
+                txt.append("\nMsg: " + msg);
+                txt.append("\nDe.: " + pct.getAddress().getHostAddress());
+                        }
+                } catch (IOException e) {
+                System.err.println("ERRO: " + e.getMessage());
+        }
+    }
+
+    static class DataAtualDoServidor {
+
+        public static LocalDateTime obterDataAtual() {
+            return LocalDateTime.now(); // Isso irá criar uma instância de LocalDateTime representando a data e hora atuais do servidor.
+        }
+    }
+
+}
---
+++ Sockets-v4/src/udp/Cliente.java
@@ -0,0 +1,37 @@
+package udp;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Cliente {
+    
+    private final String HOST = "172.25.3.124";
+    private final int PORTA = 12345;
+    //private final int PORTA2 = 54321;
+    
+    public Cliente(){
+    }
+    
+    public void enviar(String msg){
+        try {
+            //converter a mensagem em bytes
+            byte[] buffer = msg.getBytes();
+            
+            //criar o pacote da mensagem
+            DatagramPacket pct = new DatagramPacket(
+                buffer,
+                buffer.length,
+                InetAddress.getByName(HOST),
+                PORTA
+            );
+            
+            //enviar a mensagem
+            new DatagramSocket().send(pct);
+            
+        } catch (Exception e) {
+            System.err.println("ERRO: " + e.getMessage());
+        }
+    }
+    
+}
